69dd3e36b76dc45e14ca168646f611a64b17470e
LeeR <52798154+leer752@users.noreply.github.com>
2019-08-02 15:27:51 -0500

    Add files via upload

--- code/foundation/create_arrays.py
+++ code/foundation/create_arrays.py
@@ -6,20 +6,24 @@ import pygame
 # Assign positions to a list array that is 10x10 units
 def create_player_array():
     player_grid = []
+    size_margin = g_var.unit_margin + g_var.unit_size
     for row in range(10):
         player_grid.append([])
-        for column in range(10):
+        for col in range(10):
             player_grid[row].append(0)
-            player_grid[row][column] = class_Board.Board("player", g_var.white, "empty", "empty", pygame.Rect((g_var.unit_margin + g_var.unit_size) * column + 80, (g_var.unit_margin + g_var.unit_size) * row + 60, g_var.unit_size, g_var.unit_size))
+            square = [size_margin * col + 80, size_margin * row + 60, g_var.unit_size, g_var.unit_size]
+            player_grid[row][col] = class_Board.Board("player", g_var.white, "empty", "empty", pygame.Rect(square))
     return player_grid
 
 
 # Assign positions to a list array that is 10x10 units
 def create_enemy_array():
     enemy_grid = []
+    size_margin = g_var.unit_margin + g_var.unit_size
     for row in range(10):
         enemy_grid.append([])
-        for column in range(10):
+        for col in range(10):
             enemy_grid[row].append(0)
-            enemy_grid[row][column] = class_Board.Board("enemy", g_var.white, "empty", "empty", pygame.Rect((g_var.unit_margin + g_var.unit_size) * column + 600, (g_var.unit_margin + g_var.unit_size) * row + 60, g_var.unit_size, g_var.unit_size))
+            square = [size_margin * col + 600, size_margin * row + 60, g_var.unit_size, g_var.unit_size]
+            enemy_grid[row][col] = class_Board.Board("enemy", g_var.white, "empty", "empty", pygame.Rect(square))
     return enemy_grid
